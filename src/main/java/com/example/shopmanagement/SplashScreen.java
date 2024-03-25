6a1bf0b38bb59bafb9f7a52ef74c514f2724c0ff
osanchezGSU <150194031+osanchezGSU@users.noreply.github.com>
2024-03-24 23:58:28 -0400

    Project Initiation

---
+++ src/main/java/com/example/shopmanagement/SplashScreen.java
@@ -0,0 +1,4 @@
+package com.example.shopmanagement;
+
+public class SplashScreen {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.shopmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.shopmanagement to javafx.fxml;
+    exports com.example.shopmanagement;
+}
